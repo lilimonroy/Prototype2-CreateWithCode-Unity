5af5a1f25679b875e641c966b4e1170a09490c5d
lilimonroy <galadriel@comunidad.unam.mx>
2021-01-18 16:41:04 -0600

    Prototype2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float nextShootDog = 0.0f;
+    private float rateShootDog = 0.5f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextShootDog)
+        {
+            nextShootDog = Time.time + rateShootDog;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
